8891c401a73c7f80002a083a8f896b4095972939
dalespiteri <dalespiteri@gmail.com>
2017-02-26 17:59:34 -0500

    sliding effects (javascript) on the episodes div

--- script/episodeToggle.js
+++ script/episodeToggle.js
@@ -1,4 +1,8 @@
-$('.currentEpisode button').click(function () {
+$('#episodesLink button').click(function () {
 	$('.currentEpisode').toggleClass('leftOffscreen');
 	$('.episodeList').toggleClass('offscreenLeft');
+});
+
+$('.episodeListLink').click(function() {
+	$(this).next('div').slideToggle(200);
 });
