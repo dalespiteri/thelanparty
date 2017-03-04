7cc8321df5f4d190c9a69b48075fbd45a32f4667
dalespiteri <dalespiteri@gmail.com>
2017-03-04 13:27:48 -0500

    went back to old pixel font

--- script/episodeToggle.js
+++ script/episodeToggle.js
@@ -6,3 +6,25 @@ $('#episodesLink button, #backButton').click(function () {
 $('.episodeListLink').click(function() {
 	$(this).next('div').slideToggle(200);
 });
+
+//Episode List Slides//
+
+$('.epLink').click(function() {
+  $('.episodePic').removeClass('visibleLeft');
+  $('.episodeDes').removeClass('visibleRight');
+});
+
+$('#ep12link').click(function() {
+  $('#episode12des').addClass('visibleRight');
+  $('#episode12pic').addClass('visibleLeft');
+});
+
+$('#ep11link').click(function() {
+  $('#episode11des').addClass('visibleRight');
+  $('#episode11pic').addClass('visibleLeft');
+});
+
+$('#ep10link').click(function() {
+  $('#episode10des').addClass('visibleRight');
+  $('#episode10pic').addClass('visibleLeft');
+});
