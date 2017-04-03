1d71b62e0b2ec2b2c0263be9844963b38e22e3b1
dalespiteri <dalespiteri@gmail.com>
2017-04-03 01:50:08 -0400

    hovers
    added the hover on contact

--- script/episodeToggle.js
+++ script/episodeToggle.js
@@ -1,41 +1,57 @@
 $(document).ready(function() {
 
+//Intro Slides//
+
 $('.slideGoneUp').addClass('slideDown');
 $('.slideGoneDown').addClass('slideUp');
 
-	$('#episodesLink button, #backButton').click(function () {
-		$('.currentEpisode').toggleClass('moveLeft');
-		$('.episodeListcontainer').toggleClass('offscreenLeft');
-	});
+//episode slide toggle//
 
-	//Episode List Slides//
+$('#episodesLink button, #backButton').click(function () {
+	$('.currentEpisode').toggleClass('moveLeft');
+	$('.episodeListcontainer').toggleClass('offscreenLeft');
+});
 
-	$('.epLink').click(function() {
-	  $('.episodePic').removeClass('active');
-	  $('.episodeDes').removeClass('active2');
-	});
+//Episode List Slides//
 
-	$('#ep12link').click(function() {
-	  $('#episode12des').addClass('active2');
-	  $('#episode12pic').addClass('active');
-	  $('#episode12pic').appendTo($('.episodePicContainer'));
-	  $('#episode12des').appendTo($('.episodeDesContainer'));
+$('.epLink').click(function() {
+  $('.episodePic').removeClass('active');
+  $('.episodeDes').removeClass('active2');
+});
 
-	});
+$('#ep12link').click(function() {
+  $('#episode12des').addClass('active2');
+  $('#episode12pic').addClass('active');
+  $('#episode12pic').appendTo($('.episodePicContainer'));
+  $('#episode12des').appendTo($('.episodeDesContainer'));
 
-	$('#ep11link').click(function() {
-	  $('#episode11des').addClass('active2');
-	  $('#episode11pic').addClass('active');
-	  $('#episode11pic').appendTo($('.episodePicContainer'));
-	  $('#episode11des').appendTo($('.episodeDesContainer'));
+});
 
-	});
+$('#ep11link').click(function() {
+  $('#episode11des').addClass('active2');
+  $('#episode11pic').addClass('active');
+  $('#episode11pic').appendTo($('.episodePicContainer'));
+  $('#episode11des').appendTo($('.episodeDesContainer'));
 
-	$('#ep10link').click(function() {
-	  $('#episode10des').addClass('active2');
-	  $('#episode10pic').addClass('active');
-	  $('#episode10pic').appendTo($('.episodePicContainer'));
-	  $('#episode10des').appendTo($('.episodeDesContainer'));
+});
+
+$('#ep10link').click(function() {
+  $('#episode10des').addClass('active2');
+  $('#episode10pic').addClass('active');
+  $('#episode10pic').appendTo($('.episodePicContainer'));
+  $('#episode10des').appendTo($('.episodeDesContainer'));
+});
+
+//contact toggle//
+
+$('#contact')
+
+	.mouseover(function() {
+	$('#contact').text("contact@theLANparty.com");
+	})
+
+	.mouseleave(function() {
+		$('#contact').text("contact");
 	});
 
 });
